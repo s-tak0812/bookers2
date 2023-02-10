26c7bba8d19baac9d042741814bd91ddbe54cb16
s-tak0812 <122010689+s-tak0812@users.noreply.github.com>
2023-02-10 07:51:27 +0000

    [Add]gem,fas,control,model,view

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,13 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import '@fortawesome/fontawesome-free/js/all'
+
+import "jquery";
+import "popper.jp";
+import "bootstrap";
+import "../stylesheets/application"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  devise_for :users
+
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :books, only:[:index, :show, :edit]
+
 end
