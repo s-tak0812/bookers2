6b228b0a35c68f2e3ea4ea36bb68ff5dfb6e9782
s-tak0812 <122010689+s-tak0812@users.noreply.github.com>
2023-02-10 09:51:08 +0000

    [Add]Book,create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,40 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
-  def edit
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to books_path
+    else
+      render :new
+    end
   end
+
+
+  def edit
+    @book = Book.find(params[:id])
+
+  end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+
+  end
+
+
+
 end
