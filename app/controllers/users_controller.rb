2438953523a4a9b608a426c14be56d7058a3c86a
s-tak0812 <122010689+s-tak0812@users.noreply.github.com>
2023-02-13 03:06:08 +0000

    [Update]book,user--link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,15 +4,16 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
     @user = User.find(current_user[:id])
   end
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
 end
